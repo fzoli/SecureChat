653966c2ab8d92928ac109506794b9c3758a323e
Zoli <Zoli@NB-FZoli>
2014-01-28 22:38:45 +0100

    removing chat messages resets tray icon text too

--- src/org/dyndns/fzoli/chat/client/view/ChatFrame.java
+++ src/org/dyndns/fzoli/chat/client/view/ChatFrame.java
@@ -760,12 +760,11 @@ public class ChatFrame extends JFrame implements RelocalizableWindow {
      */
     public void removeChatMessages() {
         synchronized (DOC_LOCK) {
-            if (unreadedMessages != 0) {
-                unreadedMessages = 0;
-                if (!isActiveWindow()) {
-                    Main.setTrayIconNumber(1);
-                }
-            }
+            unreadedMessages = 0;
+            Main.setTrayIconNumber(0);
+//            if (!isActiveWindow()) {
+//                Main.setTrayIconNumber(1);
+//            }
             try {
                 lastSender = null;
                 lastDay = null;
